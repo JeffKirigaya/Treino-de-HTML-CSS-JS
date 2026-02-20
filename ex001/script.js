5a14103f4574c8722626c06ce28ef8ba220d8ab3
JeffKirigaya <jeffersonlucas123321@gmail.com>
2026-02-19 22:01:24 -0300

    estilo criado

---
+++ ex001/script.js
@@ -0,0 +1,9 @@
+ let texto = document.getElementById('add')
+ 
+ function adicionar() {
+    if (texto.value.length == 0) {
+        alert('Por favor, digite algo!')
+    } else {
+        
+    }
+ }
