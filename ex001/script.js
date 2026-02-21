cc7051a6ef105a1fccc213aa9ab9b76399b79089
JeffKirigaya <jeffersonlucas123321@gmail.com>
2026-02-21 19:49:51 -0300

    adicionando a lista

--- ex001/script.js
+++ ex001/script.js
@@ -1,9 +1,15 @@
  let texto = document.getElementById('add')
- 
+ let lista = document.getElementById('list')
+
  function adicionar() {
     if (texto.value.length == 0) {
         alert('Por favor, digite algo!')
     } else {
-        
+       /* let itens = document.createElement('li')
+        lista.appendChild(itens)
+        itens.innerHTML = `${texto.value}`
+        */
+       texto.innerHTML = 'teste'
     }
+    
  }
